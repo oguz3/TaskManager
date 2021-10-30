a591d81331f0c10140af82ff0308193304d8d0c7
oguz3 <ibrahimoguzhan.ulukaya@ogr.deu.edu.tr>
2021-10-30 22:33:13 +0300

    feat: init

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+import Index from "./Index";
+import "./index.css";
+
+function render() {
+	ReactDOM.render(<Index />, document.body);
+}
+
+render();
